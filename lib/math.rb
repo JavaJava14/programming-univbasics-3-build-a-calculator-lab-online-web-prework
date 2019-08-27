6758901a1a8edc658c487b836b585ff63090932e
Jose Chavira <joeychavira2016@gmail.com>
2019-08-27 19:12:18 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  add = 5 + 4
+  puts "#{num1+num2}"
+  return add
 end
 
 def subtraction(num1, num2)
-
+  subtract = 10 - 5
+  puts "#{num1-num2}"
+  return subtract
 end
 
 def division(num1, num2)
-
+  divide = 50 / 2
+  puts "#{num1/num2}"
+  return divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = 4 * 30
+  puts "#{num1*num2}"
+  return multiply
 end
 
 def modulo(num1, num2)
-
+  remainder = 34 % 5
+  puts "#{num1%num2}"
+  return remainder
 end
 
 def square_root(num)
-
+  caret = Math.sqrt(81)
+  puts "#{Math.sqrt(num)}"
+  return caret
 end
